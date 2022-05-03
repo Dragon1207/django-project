a749ca7abaa7943161fcaa9a96cbd46844563f85
Dragon1207 <smileboth0805@outlook.com>
2022-05-03 20:57:06 -0300

    Create URL for recipes

--- recipes/views.py
+++ recipes/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/pages/home.html')
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html')
