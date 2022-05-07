feef4588a75ca3296c9b75de53d8eab078b00f79
Dragon1207 <smileboth0805@outlook.com>
2022-05-07 19:53:10 -0300

    Add recipes in admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, Recipe
 
 
 class CategoryAdmin(admin.ModelAdmin):
     ...
 
 
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Recipe, RecipeAdmin)
--- recipes/models.py
+++ recipes/models.py
@@ -26,3 +26,6 @@ class Recipe(models.Model):
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
