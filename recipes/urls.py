0bc77001d32a1b8672ad3d3c4997cd7094a95f68
Dragon1207 <smileboth0805@outlook.com>
2022-05-06 21:22:25 -0300

    Insert datas in view home list

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('recipes/<int:id>/', views.recipe)
+    path('recipes/<int:id>/', views.recipe),
 ]
