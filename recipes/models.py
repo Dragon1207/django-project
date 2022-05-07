1c8362bd652dee62f53754e564646e1671237226
Dragon1207 <smileboth0805@outlook.com>
2022-05-07 18:36:54 -0300

    Apply migrations

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,9 @@
-from distutils.command.upload import upload
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
 
 
 class Recipe(models.Model):
@@ -16,3 +20,6 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
