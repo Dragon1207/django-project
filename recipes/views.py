15cd864023d23853a0f1305e41510abb046c4a4a
Dragon1207 <smileboth0805@outlook.com>
2022-05-11 10:31:40 -0300

    apply the get_list_or_404 shortcut

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 from utils.recipes.factory import make_recipe
+
 from recipes.models import Recipe
 
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes })
+        'recipes': recipes})
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
-    return render(request, 'recipes/pages/category.html', context={'recipes': recipes})
+    recipes = get_list_or_404(Recipe.objects.filter(
+        category__id=category_id, is_published=True,).order_by('-id'))
+    return render(request, 'recipes/pages/category.html',
+                  context={'recipes': recipes})
 
 
 def recipe(request, id):
