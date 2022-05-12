3d5524e887cd204bff515bd4491f52aeaeeeaa25
Dragon1207 <smileboth0805@outlook.com>
2022-05-12 16:37:38 -0300

    Create models tests structure

---
+++ recipes/tests/test_recipe_model_recipe.py
@@ -0,0 +1,7 @@
+from .test_recipe_base import RecipeTestBase
+
+
+class RecipeModelTest(RecipeTestBase):
+    def setUp(self) -> None:
+        self.recipe = self.make_recipe()
+        return super().setUp()
