bc2be14d6546f4b336671b2154e8fec4f13ea66d
Dragon1207 <smileboth0805@outlook.com>
2022-05-12 21:29:15 -0300

    Create parameterized tests with parameterized

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ValidationError
+from parameterized import parameterized
 
 from .test_recipe_base import RecipeTestBase
 
@@ -8,8 +9,14 @@ class RecipeModelTest(RecipeTestBase):
         self.recipe = self.make_recipe()
         return super().setUp()
 
-    def test_recipe_title_raises_error_if_title_has_more_than_65_chars(self):
+    @parameterized.expand([
+        ('title', 65),
+        ('description', 165),
+        ('preparation_time_unit', 65),
+        ('servings_unit', 65),
+    ])
+    def test_recipe_fields_max_length(self, field, max_length):
         self.recipe.title = 'A' * 70
-
+        setattr(self.recipe, field, 'A' * (max_length + 1))
         with self.assertRaises(ValidationError):
-            self.recipe.full_clean()  # AQUI A VALIDAÇÃO OCORRE
+            self.recipe.full_clean()
