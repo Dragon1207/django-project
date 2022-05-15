3d0e0087397df3a8ad2e865074e961247981029c
Dragon1207 <smileboth0805@outlook.com>
2022-05-14 21:31:28 -0300

    Test to ensure search_term will be escaped for security

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,5 +1,4 @@
 # from unittest import skip
-
 from django.urls import resolve, reverse
 from recipes import views
 from recipes.tests.test_recipe_base import RecipeTestBase
@@ -120,3 +119,11 @@ class RecipeViewsTest(RecipeTestBase):
     def test_recipe_search_raises_404_if_no_search_term(self):
         response = self.client.get(reverse('recipes:search'))
         self.assertEqual(response.status_code, 404)
+
+    def test_recipe_search_term_is_on_page_title_and_escaped(self):
+        url = reverse('recipes:search') + '?q=<Teste>'
+        response = self.client.get(url)
+        self.assertIn(
+            'Search for &quot;&lt;Teste&gt;&quot;',
+            response.content.decode('utf-8')
+        )
