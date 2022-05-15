1550aa90dfc25426a0029a7a2dc8c7eaeb4655f2
Dragon1207 <smileboth0805@outlook.com>
2022-05-14 21:46:44 -0300

    Separate tests from all views into separate groups

---
+++ recipes/tests/test_recipe_home_view.py
@@ -0,0 +1,57 @@
+# from unittest import skip
+from django.urls import resolve, reverse
+from recipes import views
+from recipes.tests.test_recipe_base import RecipeTestBase
+
+
+class RecipeHomeViewTest(RecipeTestBase):
+    def test_recipe_home_view_function_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
+
+    def test_category_home_view_function_is_correct(self):
+        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
+        self.assertIs(view.func, views.category)
+
+    def test_recipe_home_view_returns_status_code_200_OK(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    # O skip serve para pular o teste ou a classe com testes
+    # @skip('Working in progress - WIP')
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn(
+            '<h1>No recipes have been published yet</h1>',
+            response.content.decode('utf-8')
+        )
+        # Tenho que escrever mais código de teste aqui, então faço o teste
+        # falhar para lembrar que tenho que voltar aqui
+        # self.fail('Para lembrar que tenho de terminar o teste')
+
+    def test_recipe_home_template_loads_recipes(self):
+        self.make_recipe()
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        self.assertIn('Recipe Title', content)
+        self.assertIn('10 Minutos', content)
+        self.assertIn('5 Porções', content)
+        response_context_recipes = response.context['recipes']
+        self.assertEqual(len(response_context_recipes), 1)
+
+    def test_recipe_home_template_dont_load_recipes_not_published(self):
+        """tests if the is_published field is unchecked. 
+           That means the recipe is not published
+        """
+        self.make_recipe(is_published=False)
+
+        response = self.client.get(reverse('recipes:home'))
+
+        self.assertIn(
+            '<h1>No recipes have been published yet</h1>',
+            response.content.decode('utf-8')
+        )
