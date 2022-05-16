81f91646067e2ae62bb48b8d151deac3567dd649
Dragon1207 <smileboth0805@outlook.com>
2022-05-16 10:26:47 -0300

    Change page range when necessary

--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -12,5 +12,37 @@ class PaginationTest(TestCase):
         )
         self.assertAlmostEqual([1, 2, 3, 4], pagination)
 
-    def test_first_range_is_static_if_current_page_is_less_than_middle_page(self):
-        ...
+    def test_first_range_is_static_if_current_page_is_less_than_middle_page(self):  # noqa: E501
+        # Current page = 1 - Qty Page = 2 - Middle Page = 2
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=1,
+        )
+        self.assertEqual([1, 2, 3, 4], pagination)
+
+        # Current page = 2 - Qty Page = 2 - Middle Page = 2
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=2,
+        )
+        self.assertEqual([1, 2, 3, 4], pagination)
+
+        # Current page = 3 - Qty Page = 2 - Middle Page = 2
+        # HERE RANGE SHOULD CHANGE
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=3,
+        )
+        self.assertEqual([2, 3, 4, 5], pagination)
+
+        # Current page = 4 - Qty Page = 2 - Middle Page = 2
+        # HERE RANGE SHOULD CHANGE
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=4,
+        )
+        self.assertEqual([3, 4, 5, 6], pagination)
