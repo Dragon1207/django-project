f2382dea9ced7a7934a7dc684e0d18d9c7c06986
Dragon1207 <smileboth0805@outlook.com>
2022-05-16 20:43:56 -0300

    Create the .env file using the python-dotenv library

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,5 @@
-from django.core.paginator import Paginator
+import os
+
 from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
@@ -6,13 +7,15 @@ from utils.pagination import make_pagination
 
 from recipes.models import Recipe
 
+PER_PAGE = os.environ.get('PER_PAGE', 6)
+
 
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True,
     ).order_by('-id')
 
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
@@ -24,7 +27,7 @@ def category(request, category_id):
     recipes = get_list_or_404(Recipe.objects.filter(
         category__id=category_id, is_published=True,).order_by('-id'))
 
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': page_obj,
@@ -53,7 +56,7 @@ def search(request):
         is_published=True
     ).order_by('-id')
 
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
