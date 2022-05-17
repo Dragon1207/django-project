af1672de51d91564e588bdb38fbe4b772819ba5c
Dragon1207 <smileboth0805@outlook.com>
2022-05-17 15:01:25 -0300

    Remove flash message code from home.views

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,5 @@
 import os
 
-from django.contrib import messages
 from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
@@ -17,10 +16,6 @@ def home(request):
     ).order_by('-id')
 
     page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
-
-    messages.error(request, 'Flash message da Home')
-    messages.success(request, 'Flash message da Home')
-    messages.info(request, 'Flash message da Home')
 
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
