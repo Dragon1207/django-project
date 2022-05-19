9bb7c691abc3e04db81f3648243cd41b2b882568
Dragon1207 <smileboth0805@outlook.com>
2022-05-19 10:36:48 -0300

    Configure the form with labels, widgets, errors and field attributes

--- authors/forms.py
+++ authors/forms.py
@@ -12,3 +12,30 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
+        # Para excluir alguns campos de todos
+        # exclude = ['first_name']
+
+        labels = {
+            'username': 'Username',
+            'first_name': 'First name',
+            'last_name': 'Last name',
+            'email': 'E-mail',
+            'password': 'Password',
+        }
+        help_texts = {
+            'email': 'The e-mail must be valid',
+        }
+
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty',
+            }
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your password here'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here'
+            })
+        }
