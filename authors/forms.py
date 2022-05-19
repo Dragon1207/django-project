eb522b32febbbd2da5dcd00ab3092fdd794c8644
Dragon1207 <smileboth0805@outlook.com>
2022-05-19 11:29:12 -0300

    Override form fields

--- authors/forms.py
+++ authors/forms.py
@@ -2,7 +2,45 @@ from django import forms
 from django.contrib.auth.models import User
 
 
+def add_attr(field, attr_name, attr_new_val):
+    existing_attr = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing_attr} {attr_new_val}'.strip()
+
+
+def add_placeholder(field, placeholder_val):
+    field.widget.attrs['placeholder'] = placeholder_val
+
+
 class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['username'], 'Your username')
+        add_placeholder(self.fields['email'], 'Your email here')
+        add_placeholder(self.fields['first_name'], 'Ex.: Marshal')
+        add_placeholder(self.fields['last_name'], 'Ex.: Mori')
+
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'You password',
+        }),
+        error_messages={
+            'required': 'Password must not be empty'
+        },
+        help_text=(
+            'Password must have at least one uppercase letter,'
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
+        )
+    )
+
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repeat your password',
+        })
+    )
+
     class Meta:
         model = User
         fields = [
