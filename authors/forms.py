438c7c2e4a66634c3305ada9645cad3d9e3a81ac
Dragon1207 <smileboth0805@outlook.com>
2022-05-19 23:05:30 -0300

    Test form empty fields

--- authors/forms.py
+++ authors/forms.py
@@ -37,8 +37,23 @@ class RegisterForm(forms.ModelForm):
         add_placeholder(self.fields['password'], 'Your password')
         add_placeholder(self.fields['password2'], 'Repeat your password')
 
+    first_name = forms.CharField(
+        error_messages={'required': 'Write your first name'},
+        label='First name'
+    )
+
+    last_name = forms.CharField(
+        error_messages={'required': 'Write your last name'},
+        label='Last name'
+    )
+
+    email = forms.EmailField(
+        error_messages={'required': 'E-mail is required'},
+        label='E-mail',
+        help_text='The e-mail must be valid.'
+    )
+
     password = forms.CharField(
-        required=True,
         widget=forms.PasswordInput(),
         error_messages={
             'required': 'Password must not be empty'
@@ -53,9 +68,11 @@ class RegisterForm(forms.ModelForm):
     )
 
     password2 = forms.CharField(
-        required=True,
         widget=forms.PasswordInput(),
-        label='Password2'
+        label='Password2',
+        error_messages={
+            'required': 'Please, repeat your password'
+        },
     )
 
     class Meta:
@@ -70,15 +87,7 @@ class RegisterForm(forms.ModelForm):
 
         labels = {
             'username': 'Username',
-            'first_name': 'First name',
-            'last_name': 'Last name',
-            'email': 'E-mail',
-
         }
-        help_texts = {
-            'email': 'The e-mail must be valid',
-        }
-
         error_messages = {
             'username': {
                 'required': 'This field must not be empty',
