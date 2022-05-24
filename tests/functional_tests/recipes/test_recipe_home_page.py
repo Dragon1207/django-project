2c6412bba051565af8ad31ba0ef06ed4885385b6
Dragon1207 <smileboth0805@outlook.com>
2022-05-24 10:56:52 -0300

    Leverage code that generates one revenue to generate multiple

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 from selenium.webdriver.common.by import By
 from tests.functional_tests.recipes.base import RecipeBaseFunctionalTest
@@ -7,6 +9,7 @@ from recipes.tests.test_recipe_base import RecipeMixin
 
 @pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest, RecipeMixin):
+    @patch('recipes.views.PER_PAGE', new=2)
     def test_recipe_home_page_without_recipes_not_found_message(self):
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
