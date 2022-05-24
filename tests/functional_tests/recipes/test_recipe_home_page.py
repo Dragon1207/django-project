3b6b561348ea87b8ef45dc49a9c70600f2ed2876
Dragon1207 <smileboth0805@outlook.com>
2022-05-24 14:00:07 -0300

    Test recipe home page pagination

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -2,6 +2,7 @@ from unittest.mock import patch
 
 import pytest
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from tests.functional_tests.recipes.base import RecipeBaseFunctionalTest
 
 from recipes.tests.test_recipe_base import RecipeMixin
@@ -14,3 +15,55 @@ class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest, RecipeMixin):
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
         self.assertIn('No recipes have been published yet', body.text)
+
+    @patch('recipes.views.PER_PAGE', new=2)
+    def test_recipe_search_input_can_find_correct_recipes(self):
+        recipes = self.make_recipe_in_batch()
+
+        title_needed = 'This is what I need'
+
+        recipes[0].title = title_needed
+        recipes[0].save()
+
+        # Usuário abre a página
+        self.browser.get(self.live_server_url)
+
+        # Vê um campo de busca com o texto 'Search for a recipe'
+        search_input = self.browser.find_element(
+            By.XPATH,
+            '//input[@placeholder="Search for a recipe"]'
+        )
+
+        # clica no input e digita o termo de busca
+        # para encontrar a receita com o título desejado
+        search_input.send_keys(recipes[0].title)
+        search_input.send_keys(Keys.ENTER)
+
+        # Aparece para o usuário a receita que estava procurando.
+        self.assertIn(
+            title_needed,
+            self.browser.find_element(By.CLASS_NAME, 'main-content-list').text,
+        )
+        self.sleep(6)
+
+    @patch('recipes.views.PER_PAGE', new=2)
+    def test_recipe_home_page_pagination(self):
+        self.make_recipe_in_batch()
+
+        # Usuário abre a página
+        self.browser.get(self.live_server_url)
+
+        # Vê que tem uma paginação e clica na página 2
+        page2 = self.browser.find_element(
+            By.XPATH,
+            '//a[@aria-label="Go to page 2"]'
+        )
+        page2.click()
+
+        # Vê que tem mais 2 receitas na página 2
+        self.assertEqual(
+            len(self.browser.find_elements(By.CLASS_NAME, 'recipe')),
+            2
+        )
+
+        self.sleep(6)
