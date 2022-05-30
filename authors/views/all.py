3996d7f96e03ed8ac4d541003d524dcce1321f5d
Dragon1207 <smileboth0805@outlook.com>
2022-05-30 15:42:53 -0300

    Add login required in DashboardRecipe class based view

--- authors/views/all.py
+++ authors/views/all.py
@@ -101,35 +101,6 @@ def dashboard(request):
         })
 
 
-# @login_required(login_url='authors:login', redirect_field_name='next')
-# def dashboard_recipe_new(request):
-#     form = AuthorRecipeForm(
-#         data=request.POST or None,
-#         files=request.FILES or None,
-#     )
-
-#     if form.is_valid():
-#         recipe = form.save(commit=False)
-#         recipe.author = request.user
-#         recipe.preparation_steps_is_html = False
-#         recipe.is_publised = False
-
-#         recipe.save()
-
-#         messages.success(request, 'Sua receita foi criada com sucesso!')
-#         return redirect(
-#             reverse('authors:dashboard_recipe_edit', args=(recipe.id,))
-#         )
-
-#     return render(
-#         request,
-#         'authors/pages/dashboard_recipe.html',
-#         context={
-#             'form': form,
-#             'form_action': reverse('authors:dashboard_recipe_new')
-#         })
-
-
 @login_required(login_url='authors:login', redirect_field_name='next')
 def dashboard_recipe_delete(request):
     if not request.POST:
