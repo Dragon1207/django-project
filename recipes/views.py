ab31beba52f2a340292b8455414675a778c0ee75
Dragon1207 <smileboth0805@outlook.com>
2022-05-31 13:14:48 -0300

    Create RecipeDetail class based view

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,7 @@ import os
 from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 from utils.pagination import make_pagination
 
 from recipes.models import Category, Recipe
@@ -95,7 +95,16 @@ class RecipeListViewSearch(RecipeListViewBase):
         return ctx
 
 
-def recipe(request, id):
-    recipe = get_object_or_404(Recipe, id=id, is_published=True)
-    return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': recipe, 'is_detail_page': True})
+class RecipeDetail(DetailView):
+    model = Recipe
+    context_object_name = 'recipe'
+    template_name = 'recipes/pages/recipe-view.html'
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+
+        ctx.update({
+            'is_detail_page': True
+        })
+
+        return ctx
