c3cdfde01015e0fec95ab96de6ce3c3a40a46ec3
Dragon1207 <smileboth0805@outlook.com>
2022-05-31 15:33:09 -0300

    Project update to use Postgres on server

--- recipes/tests/test_recipe_detail_view.py
+++ recipes/tests/test_recipe_detail_view.py
@@ -7,12 +7,12 @@ from recipes.tests.test_recipe_base import RecipeTestBase
 class RecipeDetailViewTest(RecipeTestBase):
 
     def test_recipe_detail_view_function_is_correct(self):
-        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
-        self.assertIs(view.func, views.recipe)
+        view = resolve(reverse('recipes:recipe', kwargs={'pk': 1}))
+        self.assertIs(view.func.view_class, views.RecipeDetail)
 
     def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
         response = self.client.get(
-            reverse('recipes:recipe', kwargs={'id': 1000}))
+            reverse('recipes:recipe', kwargs={'pk': 1000}))
         self.assertEqual(response.status_code, 404)
 
     def test_recipe_detail_template_loads_the_correct_recipe(self):
@@ -21,7 +21,7 @@ class RecipeDetailViewTest(RecipeTestBase):
         self.make_recipe(title=needed_title)
 
         response = self.client.get(
-            reverse('recipes:recipe', kwargs={'id': 1}))
+            reverse('recipes:recipe', kwargs={'pk': 1}))
         content = response.content.decode('utf-8')
 
         # Check if one recipe exists
@@ -31,6 +31,6 @@ class RecipeDetailViewTest(RecipeTestBase):
         recipe = self.make_recipe(is_published=False)
         response = self.client.get(
             reverse('recipes:recipe',
-                    kwargs={'id': recipe.id}))
+                    kwargs={'pk': recipe.id}))
 
         self.assertEqual(response.status_code, 404)
--- recipes/views.py
+++ recipes/views.py
@@ -2,11 +2,10 @@ import os
 
 from django.db.models import Q
 from django.http import Http404
-from django.shortcuts import get_list_or_404, get_object_or_404, render
 from django.views.generic import DetailView, ListView
 from utils.pagination import make_pagination
 
-from recipes.models import Category, Recipe
+from recipes.models import Recipe
 
 PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
@@ -99,6 +98,13 @@ class RecipeDetail(DetailView):
     model = Recipe
     context_object_name = 'recipe'
     template_name = 'recipes/pages/recipe-view.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(
+            is_published=True,
+        )
+        return qs
 
     def get_context_data(self, *args, **kwargs):
         ctx = super().get_context_data(*args, **kwargs)
