6d5f15bbdc0fc66386f26a688a84fcbc29dd4c11
Dragon1207 <smileboth0805@outlook.com>
2022-05-31 11:12:49 -0300

    Create RecipeListViewSearch class based view

--- recipes/views.py
+++ recipes/views.py
@@ -54,7 +54,28 @@ class RecipeListViewCategory(RecipeListViewBase):
 
 
 class RecipeListViewSearch(RecipeListViewBase):
-    ...
+    def get_queryset(self, *args, **kwargs):
+        search_term = self.request.GET.get('q', '')
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ),
+            is_published=True,
+        )
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        search_term = self.request.GET.get('q', '')
+
+        ctx.update({
+            'page_title': f'Search for "{search_term}" |',
+            'search_term': search_term,
+            'additional_url_query': f'&q={search_term}',
+        })
+
+        return ctx
 
 
 def home(request):
