fef52c88477933c26c336b7b2a5a86931c580d2b
Dragon1207 <smileboth0805@outlook.com>
2022-06-01 21:52:34 -0300

    Create menu structure

--- recipes/urls.py
+++ recipes/urls.py
@@ -25,4 +25,9 @@ urlpatterns = [
         views.RecipeDetailAPI.as_view(),
         name="recipes_api_v1_detail",
     ),
+    path(
+        'recipes/theory/',
+        views.theory,
+        name="theory",
+    ),
 ]
