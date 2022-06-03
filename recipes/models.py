992df3657ed719c4ac57e26a6242204b55109f28
Dragon1207 <smileboth0805@outlook.com>
2022-06-03 15:59:18 -0300

    Resize images with Pillow

--- recipes/models.py
+++ recipes/models.py
@@ -1,8 +1,12 @@
+import os
+
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
+from PIL import Image
 
 
 class Category(models.Model):
@@ -39,12 +43,39 @@ class Recipe(models.Model):
     def get_absolute_url(self):
         return reverse("recipes:recipe", args=(self.id,))
 
+    @staticmethod
+    def resize_image(image, new_width=800):
+        image_full_path = os.path.join(settings.MEDIA_ROOT, image.name)
+        image_pillow = Image.open(image_full_path)
+        original_width, original_height = image_pillow.size
+
+        if original_width <= new_width:
+            image_pillow.close()
+            return
+
+        new_height = round((new_width * original_height) / original_width)
+
+        new_image = image_pillow.resize((new_width, new_height), Image.LANCZOS)
+        new_image.save(
+            image_full_path,
+            optimize=True,
+            quality=50,
+        )
+
     def save(self, *args, **kwargs):
         if not self.slug:
             slug = f'{slugify(self.title)}'
             self.slug = slug
 
-        return super().save(*args, **kwargs)
+        saved = super().save(*args, **kwargs)
+
+        if self.cover:
+            try:
+                self.resize_image(self.cover, 840)
+            except FileNotFoundError:
+                ...
+
+        return saved
 
     class Meta:
         verbose_name = _('Recipe')
