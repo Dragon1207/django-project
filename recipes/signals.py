09b93824c0f0d56a57e441f8bc356ff32389b01c
Dragon1207 <smileboth0805@outlook.com>
2022-06-03 14:56:50 -0300

    Conditionally delete old images

--- recipes/signals.py
+++ recipes/signals.py
@@ -1,6 +1,6 @@
 import os
 
-from django.db.models.signals import pre_delete
+from django.db.models.signals import pre_delete, pre_save
 from django.dispatch import receiver
 
 from recipes.models import Recipe
@@ -15,5 +15,20 @@ def delete_cover(instance):
 
 @receiver(pre_delete, sender=Recipe)
 def recipe_cover_delete(sender, instance, *args, **kwargs):
-    old_instance = Recipe.objects.get(pk=instance.pk)
-    delete_cover(old_instance)
+    old_instance = Recipe.objects.filter(pk=instance.pk).first()
+
+    if old_instance:
+        delete_cover(old_instance)
+
+
+@receiver(pre_save, sender=Recipe)
+def recipe_cover_update(sender, instance, *args, **kwargs):
+    old_instance = Recipe.objects.filter(pk=instance.pk).first()
+
+    if not old_instance:
+        return
+
+    is_new_cover = old_instance.cover != instance.cover
+
+    if is_new_cover:
+        delete_cover(old_instance)
