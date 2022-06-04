92ec335b9aed1a6b3cc6c152d451bb315183c4cf
Dragon1207 <smileboth0805@outlook.com>
2022-06-04 17:08:21 -0300

    Create the log of http GET and POST methods in recipe_api_list

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,19 +1,23 @@
 from django.shortcuts import get_object_or_404
 from recipes.models import Recipe
 from recipes.serializers import RecipeSerializer
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
-@api_view()
+@api_view(http_method_names=['get', 'post'])
 def recipe_api_list(request):
-    recipes = Recipe.objects.get_published()[:10]
-    serializer = RecipeSerializer(
-        instance=recipes,
-        many=True,
-        context={'request': request},
-    )
-    return Response(serializer.data)
+    if(request.method == 'GET'):
+        recipes = Recipe.objects.get_published()[:10]
+        serializer = RecipeSerializer(
+            instance=recipes,
+            many=True,
+            context={'request': request},
+        )
+        return Response(serializer.data)
+    else:
+        return Response('POST', status=status.HTTP_201_CREATED)
 
 
 @api_view()
