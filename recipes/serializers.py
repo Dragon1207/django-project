8845b1db1017e16b2e189de5f161d86ed9a7f635
Dragon1207 <smileboth0805@outlook.com>
2022-06-04 14:59:58 -0300

    Change serializer to ModelSerializer

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,19 +1,23 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+from recipes.models import Recipe
 
-class RecipeSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=65)
-    description = serializers.CharField(max_length=165)
-    public = serializers.BooleanField(source='is_published')
+
+class RecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipe
+        fields = [
+            'id', 'title', 'description', 'author',
+            'category', 'public', 'preparation'
+        ]
+
+    # Abaixo, ficam só os campos personalizados
+    public = serializers.BooleanField(source='is_published', read_only=True)
     preparation = serializers.SerializerMethodField(
-        method_name='any_method_name'
+        method_name='any_method_name', read_only=True
     )
-    category = serializers.StringRelatedField()
-    author = serializers.PrimaryKeyRelatedField(
-        queryset=User.objects.all()
-    )
+    category = serializers.StringRelatedField(read_only=True,)
 
     def any_method_name(self, recipe):
         return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
