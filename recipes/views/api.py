d37ab4df70ef9bd8bc18c4762a5db9feac84c555
Dragon1207 <smileboth0805@outlook.com>
2022-06-06 16:45:46 -0300

    Fix name method

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -15,7 +15,7 @@ class RecipeAPIv2ViewSet(ModelViewSet):
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Pagination
 
-    def patch(self, request, *args, **kwargs):
+    def parcial_update(self, request, *args, **kwargs):
         pk = kwargs.get('pk')
         recipe = self.get_queryset().filter(pk=pk).first()
         serializer = RecipeSerializer(
